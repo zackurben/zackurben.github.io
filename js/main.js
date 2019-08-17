6f88a60883b0ec5490b1477f26ddf3fa8c9aed3d
Zack Urben <zackurben@gmail.com>
2019-08-17 15:17:28 -0500

    Rewiting site js.

--- js/main.js
+++ js/main.js
@@ -7,10 +7,13 @@
  *   The time this skill was acquired.
  * @param stop {Number} optional
  *   The time this skill was no longer being used (EPOCH in ms).
+ * 
+ * @returns {Number}
+ *   The time in years.
  */
-var timeFrom = function(start, stop) {
-  var time = stop ? stop : (new Date()).getTime();
-  var delta = parseFloat(time - start);
+const since = (start, stop) => {
+  const time = stop ? stop : (new Date()).getTime();
+  let delta = parseFloat(time - start);
 
   // ms to years
   delta = delta / (1000 * 60 * 60 * 24 * 365);
@@ -18,45 +21,42 @@ var timeFrom = function(start, stop) {
 };
 
 // Dynamic list of skills.
-var skills = _.sortByOrder([
+const skills = [
   {type: 'Java', personal: 8, professional: 0},
-  {type: 'HTML5', personal: timeFrom(1262325600000), professional: timeFrom(1372654800000)},
-  {type: 'SQL', personal: timeFrom(1262325600000, 1425189600000), professional: timeFrom(1372654800000, 1425189600000)},
-  {type: 'CSS3', personal: timeFrom(1262325600000), professional: timeFrom(1372654800000)},
-  {type: 'JavaScript', personal: timeFrom(1343797200000), professional: timeFrom(1393653600000)},
-  {type: 'Node.js', personal: timeFrom(1420092000000), professional: timeFrom(1430456400000)},
-  {type: 'UNIX', personal: timeFrom(1343797200000), professional: timeFrom(1372654800000)},
-  {type: 'PHP', personal: timeFrom(1262325600000, 1425189600000), professional: timeFrom(1372654800000, 1425189600000)},
-  {type: 'Git', personal: timeFrom(1343797200000), professional: timeFrom(1393653600000)},
-  {type: 'MongoDB', personal: timeFrom(1393653600000), professional: timeFrom(1393653600000)},
-  {type: 'Redis', personal: timeFrom(1393653600000), professional: timeFrom(1393653600000)}
-], ['professional'], ['desc']);
-
-// On page load, register scroll events and create out Chart.
-window.onload = function() {
-  var navHeight = $('nav').height();
-  var paddingHeight = parseInt($('.content').css('padding-top').split('p')[0]);
-  var scrollOffset = Math.abs(paddingHeight - navHeight);
-
-  // Hack for small-width devices, to fix nav height issues, due to dynamic DOM elements.
-  if ($(window).width() < 728) {
-    scrollOffset = Math.abs(paddingHeight + navHeight);
+  {type: 'HTML5', personal: since(1262325600000), professional: since(1372654800000)},
+  {type: 'SQL', personal: since(1262325600000, 1425189600000), professional: since(1372654800000, 1425189600000)},
+  {type: 'CSS3', personal: since(1262325600000), professional: since(1372654800000)},
+  {type: 'JavaScript', personal: since(1343797200000), professional: since(1393653600000)},
+  {type: 'Node.js', personal: since(1420092000000), professional: since(1430456400000)},
+  {type: 'UNIX', personal: since(1343797200000), professional: since(1372654800000)},
+  {type: 'PHP', personal: since(1262325600000, 1425189600000), professional: since(1372654800000, 1425189600000)},
+  {type: 'Git', personal: since(1343797200000), professional: since(1393653600000)},
+  {type: 'MongoDB', personal: since(1393653600000), professional: since(1393653600000)},
+  {type: 'Redis', personal: since(1393653600000), professional: since(1393653600000)}
+].sort((first, second) => {
+  if (first.professional == second.professional) {
+    return 0;
   }
 
+  return first.professional > second.professional 
+    ? -1
+    : 1
+});
+
+// On page load create out Chart.
+window.onload = () => {
   new Chartist.Line('.ct-chart', {
-    labels: _.pluck(skills, 'type'),
+    labels: skills.map(s => s.type),
     series: [
-      _.pluck(skills, 'personal'),
-      _.pluck(skills, 'professional')
+      skills.map(s => s.personal),
+      skills.map(s => s.professional)
     ]
   }, {
     low: 0,
     showArea: true,
     axisY: {
       offset: 80,
-      labelInterpolationFnc: function(value) {
-        return value + ' Years'
-      }
+      labelInterpolationFnc: value => `${value} Years`
     }
   });
 };
